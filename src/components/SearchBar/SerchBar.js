ee1c1e1ce14f154e1d96c92574e3498800c1ee91
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-08-13 20:04:40 +0100

    Add SearchBar component with css styling.

---
+++ src/components/SearchBar/SerchBar.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import "./SearchBar.css";
+
+const sortByOptions = {
+  "Best Match": "best_match",
+  "Highest Rated": "rating",
+  "Most Reviewed": "review_count",
+};
+
+class SearchBar extends Component {
+  renderSortByOptions() {
+    return Object.keys(sortByOptions).map((option) => {
+      let optionValue = sortByOptions[option];
+      return <li key={optionValue}>{option}</li>;
+    });
+  }
+
+  render() {
+    return (
+      <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>{this.renderSortByOptions}</ul>
+        </div>
+
+        <div className="SearchBar-fields">
+          <input placeholder="Search Buisnesses" />
+          <input placeholder="Where?" />
+        </div>
+
+        <div className="SearchBar-submit">
+          <button>Let's go</button>{" "}
+          {/*was <a> </a> with no href attribute instead of button...*/}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
